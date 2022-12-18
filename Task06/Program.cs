001e58039b926b92fa9ace8966e1e690b8807b17
Ramazan Daminov <radamin@yandex.ru>
2022-12-18 18:29:06 +0300

    Added Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+{
+
+  int num;
+
+  Console.Write("Enter the number: ");
+
+  num = Convert.ToInt32(Console.ReadLine());
+
+  if (num % 2 == 0)
+
+  {
+
+    Console.Write("is: even");
+
+    Console.Read();
+
+  }
+
+  else
+
+  {
+
+    Console.Write("is: odd");
+
+    Console.Read();
+
+  }
+
+}
